a7fe2702b54d4e37a3d846e623c40d7242d35b18
Erin Capuchino <valleerin4@gmail.com>
2024-02-06 11:53:12 +0800

    dashboard frontend done

--- client/components/common/Button.jsx
+++ client/components/common/Button.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
-function Button({type,text}) {
+function Button({type,children}) {
     const buttonStyles = (type) => {
         switch (type) {
             case 'primary':
                 return 'bg-[#00FFF0] mt-6 font-semibold py-3 px-10 rounded-full max-w-[356px]'
             case 'secondary':
                 return 'bg-[#CD53EC] py-2 px-8 rounded-full'
+            case 'border-primary': 
+                return 'border-2 border-[#00FFF0] mt-6 font-semibold py-2 px-4 rounded-full max-w-[356px]'
+            case 'border-secondary':
+                return 'border-2 border-[#CD53EC] py-2 px-4 rounded-full'
             default:
                 return 'bg-[#00FFF0] mt-6 font-semibold py-2 px-4 rounded-full max-w-[356px]'
         }
     }
   return (
     <button className={buttonStyles(type)}>
-        {text}
+        {children}
     </button>
   )
 }
--- client/components/dashboard/DashboardPage.jsx
+++ client/components/dashboard/DashboardPage.jsx
@@ -1,10 +1,26 @@
 'use client'
-
+import { Button } from ".."
+import Table from "./Table"
 function DashboardPage() {
 
   return (
-    <div className="pt-28">
+    <div className="pt-28 px-3 flex flex-col gap-4 justify-center items-center">
       <h1>Hi User</h1>
+      <div className="flex flex-col gap-2 max-w-72">
+        <Button type="secondary">
+          Schedule Appointment
+        </Button>
+        <Button type="border-secondary">
+          Add Pet
+        </Button>
+      </div>
+      <div>
+        <h2 className="text-lg">Appointments</h2>
+        <div className="flex items-center justify-center h-full">
+          <Table />  
+        </div>
+        
+      </div>
     </div>
   )
 }
